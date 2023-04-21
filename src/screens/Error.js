6e8b23965818bbd9fcf675da6844857d95bfcad2
Alexis Martel <alexis.martel.w@hotmail.com>
2023-04-21 16:19:44 +0200

    🔨 Update disposition of footer & header

--- src/screens/Error.js
+++ src/screens/Error.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header } from "../components/Header";
+import { Header } from "../components/header/Header";
 import { Link } from "react-router-dom";
 
 export const Error = () => {
